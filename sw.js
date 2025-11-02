e2b862c5a80e0405f1fd9c377a17eeaa34bc5fc9
Tamana Magar <magartamana64@gmail.com>
2025-11-02 14:58:41 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@ self.addEventListener('install', event => {
 event.waitUntil((async () => {
 const cache = await caches.open(CACHE_NAME);
 cache.addAll([
-'./',
-'./converter.js',
-'./converter.css'
+'/',
+'/converter.js',
+'/converter.css'
 ]);
 })());
 });
